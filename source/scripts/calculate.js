94170f61b4a3a5b245256ddeedd4c7934255043c
Paulooo0 <paulohenrique.n.s00@gmail.com>
2024-02-17 20:16:29 -0300

    ADD: exercicio_gulp

---
+++ source/scripts/calculate.js
@@ -0,0 +1,11 @@
+function calculate(num1, operation, num2) {
+  if (operation === "+") {
+    return num1 + num2
+  } else if (operation === "-") {
+    return num1 - num2
+  } else if (operation === "*") {
+    return num1 * num2
+  } else if (operation === "/") {
+    return num1 / num2
+  }
+}
